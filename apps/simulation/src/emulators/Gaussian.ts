8d4bac5eea61525b8c77f159c56c09aaa9c46b0c
Zhi.Huang <zh.impact@outlook.com>
2023-08-11 22:40:40 +0800

    update(app): [simulation] 2d gausian

--- apps/simulation/src/emulators/Gaussian.ts
+++ apps/simulation/src/emulators/Gaussian.ts
@@ -4,22 +4,22 @@ import { Emulator } from "../types";
 
 export class Gaussian implements Emulator {
   private x = 0;
+  private y = 0;
 
   constructor(private sk: P5) {
-    sk.resizeCanvas(sk.windowWidth / 1.5, sk.windowHeight / 15);
+    sk.resizeCanvas(sk.windowWidth / 1.5, sk.windowHeight / 2);
   }
 
   update() {
     const randomGaussian = this.sk.randomGaussian.bind(this.sk);
 
-    const mean = this.sk.width / 2;
-    const sd = 100;
-    this.x = randomGaussian(mean, sd);
+    this.x = randomGaussian(this.sk.width / 2, 60);
+    this.y = randomGaussian(this.sk.height / 2, 30);
   }
 
   draw() {
     this.sk.noStroke();
     this.sk.fill(0, 10);
-    this.sk.ellipse(this.x, this.sk.height / 2, 20, 20);
+    this.sk.ellipse(this.x, this.y, 20, 20);
   }
 }
