d91aad4b59b967995158d7c73a7e83fab23c97b6
Zhi.Huang <zh.impact@outlook.com>
2023-08-12 14:00:54 +0800

    feat(simulation): add montecarlo

--- apps/simulation/src/utils/index.ts
+++ apps/simulation/src/utils/index.ts
@@ -3,3 +3,14 @@ export function mount(fn: (node: HTMLElement) => void) {
 
   fn(mountNode);
 }
+
+export function montecarlo() {
+  while (true) {
+    const r1 = Math.random();
+    const p = r1;
+    const r2 = Math.random();
+    if (r2 < p) {
+      return r1;
+    }
+  }
+}
